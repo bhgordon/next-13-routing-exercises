fa3e843ea0ec4cbc7860a3d7d5134ceaf8cfb60c
Ben Gordon <benharrisgordon@gmail.com>
2024-01-27 21:48:53 +0100

    Solved exercises (kinda, not the Toast one)

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
-import ScreenSaver from '../../../components/ScreenSaver';
-
-function ScreenSaverExercise() {
+function ScreenSaverIndexPage() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your color:</p>
+      <ul>
+        <li>
+          <Link href="/exercises/01-screensaver/blue">Blue</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/red">Red</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/violet">Violet</Link>
+        </li>
+        <li>
+          <Link href="/exercises/01-screensaver/white">White</Link>
+        </li>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverIndexPage;
